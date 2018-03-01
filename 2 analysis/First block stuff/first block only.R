bdf051aed0ea706b6e0c55f7b7fc43e154518fd1
npm27 <35810320+npm27@users.noreply.github.com>
2018-03-01 16:50:38 -0600

    more updates
    put together a script examining hpy2 and hyp3 using data from only the first block in each survey

---
+++ 2 analysis/First block stuff/first block only.R
@@ -0,0 +1,336 @@
+##setup
+dat = read.csv("first block analysis.csv")
+colnames(dat)[2] = "Judgment"
+
+p.value = function(x){
+  if (x < .001) { return("< .001")}
+  else { return(apa(x, 3, F))}
+}
+
+##Libraries
+library(cowplot)
+library(MOTE)
+library(nlme)
+library(MuMIn)
+library(lme4)
+
+options(scipen = 999)
+
+####data screening####
+##accuracy
+summary(dat)
+
+##missing data
+table("judge" = is.na(dat$Judged.Value), "recall" = is.na(dat$Recall))
+
+##outliers for judgments and recall
+mahal = mahalanobis(dat[ , c(4,5)],
+                    colMeans(dat[ , c(4,5)], na.rm = TRUE),
+                    cov(dat[ , c(4,5)], use = "pairwise.complete.obs"))
+cutoff = qchisq(1-.001, ncol(dat[ , c(4,5)]))
+cutoff;ncol(dat[ , c(4,5)])
+summary(mahal < cutoff)
+noout = subset(dat, mahal < cutoff)
+
+##additivity
+cor(noout[ , 4:8], use = "pairwise.complete.obs")
+
+####descriptive statistics####
+meanJno = tapply(noout$Judged.Value, noout$Judgment, mean, na.rm = T)
+meanRno = tapply(noout$Recall, noout$Judgment, mean, na.rm = T)
+sdJno = tapply(noout$Judged.Value, noout$Judgment, sd, na.rm = T)
+sdRno = tapply(noout$Recall, noout$Judgment, sd, na.rm = T)
+
+meanJno;meanRno
+sdJno;sdRno
+
+####hyp2 -- judgments####
+noout$ZCOS = scale(noout$COS, scale = F)
+noout$ZLSA = scale(noout$LSA, scale = F)
+noout$ZFSG = scale(noout$FSG, scale = F)
+
+##overall model
+overallh2 = lme(Judged.Value2 ~ Judgment + 
+                  ZCOS * ZLSA * ZFSG, 
+                data = noout, 
+                method = "ML", 
+                na.action = "na.omit",
+                random = ~1|Partno)
+
+summary(overallh2)
+
+####moderation stuff judgments####
+##setup
+noout$ZCOS_low = noout$ZCOS + sd(noout$ZCOS, na.rm = TRUE)
+noout$ZCOS_high = noout$ZCOS - sd(noout$ZCOS, na.rm = TRUE)
+
+##low cosine
+lowcos = lme(Judged.Value2 ~ Judgment +
+               ZCOS_low * ZLSA * ZFSG, 
+             data = noout, 
+             method = "ML", 
+             na.action = "na.omit",
+             random = ~1|Partno)
+summary(lowcos)
+
+##high cosine
+highcos = lme(Judged.Value2 ~ Judgment +
+                ZCOS_high * ZLSA * ZFSG, 
+              data = noout, 
+              method = "ML", 
+              na.action = "na.omit",
+              random = ~1|Partno)
+summary(highcos)
+
+####examine the two way interactions by splitting LSA####
+##setup
+noout$ZLSA_low = noout$ZLSA + sd(noout$ZLSA, na.rm = TRUE)
+noout$ZLSA_high = noout$ZLSA - sd(noout$ZLSA, na.rm = TRUE)
+
+##low cosine, low lsa
+lowcoslowlsa = lme(Judged.Value2 ~ Judgment +
+                     ZCOS_low * ZLSA_low * ZFSG, 
+                   data = noout, 
+                   method = "ML", 
+                   na.action = "na.omit",
+                   random = ~1|Partno)
+summary(lowcoslowlsa)
+
+##low cos, high lsa
+lowcoshighlsa = lme(Judged.Value2 ~ Judgment +
+                      ZCOS_low  * ZLSA_high * ZFSG, 
+                    data = noout, 
+                    method = "ML", 
+                    na.action = "na.omit",
+                    random = ~1|Partno)
+summary(lowcoshighlsa)
+
+##average cos, low lsa
+agcoslowlsa = lme(Judged.Value2 ~ Judgment +
+                    ZCOS * ZLSA_low * ZFSG, 
+                  data = noout, 
+                  method = "ML", 
+                  na.action = "na.omit",
+                  random = ~1|Partno)
+summary(agcoslowlsa)
+
+##average cos, high lsa
+avgcoshighlsa = lme(Judged.Value2 ~ Judgment +
+                      ZCOS  * ZLSA_high * ZFSG, 
+                    data = noout, 
+                    method = "ML", 
+                    na.action = "na.omit",
+                    random = ~1|Partno)
+summary(avgcoshighlsa)
+
+##highcos low lsa
+highcoslowlsa = lme(Judged.Value2 ~ Judgment +
+                      ZCOS_high * ZLSA_low * ZFSG, 
+                    data = noout, 
+                    method = "ML", 
+                    na.action = "na.omit",
+                    random = ~1|Partno)
+summary(highcoslowlsa)
+
+##high cos high lsa
+highcoshighlsa = lme(Judged.Value2 ~ Judgment +
+                       ZCOS_high  * ZLSA_high * ZFSG, 
+                     data = noout, 
+                     method = "ML", 
+                     na.action = "na.omit",
+                     random = ~1|Partno)
+summary(highcoshighlsa)
+
+#####moderation graphs -- judgments####
+library(ggplot2)
+
+cleanup = theme(panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(), 
+                panel.background = element_blank(), 
+                axis.line.x = element_line(color = "black"),
+                axis.line.y = element_line(color = "black"),
+                legend.key = element_rect(fill = "white"),
+                text = element_text(size = 15))
+
+dat = noout
+
+##low cos
+plot1 = ggplot(dat, aes(x = ZCOS_low, y = Judged.Value2)) +
+  labs(x = "ZFSG", y = "Judgments") +
+  scale_size_continuous(guide = FALSE) +
+  geom_abline(aes(intercept = .607, slope = .663, linetype = "-1SD ZLSA")) +
+  geom_abline(aes(intercept = .632, slope = .375, linetype = "Average ZLSA")) +
+  geom_abline(aes(intercept = .657, slope = .087, linetype = "+1SD ZLSA")) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid"),
+                        breaks = c("-1SD ZLSA", "Average ZLSA", "+1SD ZLSA"),
+                        name = "Simple Slope") +
+  coord_cartesian(xlim = c(-.20, .60), ylim = c(.1, 1)) +
+  geom_vline(xintercept = -.30) +
+  geom_hline(yintercept = 0) +
+  cleanup + 
+  labs(title="Low ZCOS") 
+
+##avg cos
+plot2 = ggplot(dat, aes(x = ZCOS_low, y = Judged.Value2)) +
+  labs(x = "ZFSG", y = "Judgments") +
+  scale_size_continuous(guide = FALSE) +
+  geom_abline(aes(intercept = .586, slope = .381, linetype = "-1SD ZLSA")) +
+  geom_abline(aes(intercept = .603, slope = .271, linetype = "Average ZLSA")) +
+  geom_abline(aes(intercept = .621, slope = .161, linetype = "+1SD ZLSA")) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid"),
+                        breaks = c("-1SD ZLSA", "Average ZLSA", "+1SD ZLSA"),
+                        name = "Simple Slope") +
+  coord_cartesian(xlim = c(-.20, .60), ylim = c(.1, 1)) +
+  geom_vline(xintercept = -.30) +
+  geom_hline(yintercept = 0) +
+  cleanup + 
+  labs(title="Average ZCOS") 
+
+##high cos
+plot3 = ggplot(dat, aes(x = ZCOS_low, y = Judged.Value2)) +
+  labs(x = "ZFSG", y = "Judgments") +
+  scale_size_continuous(guide = FALSE) +
+  geom_abline(aes(intercept = .564, slope = .099, linetype = "-1SD ZLSA")) +
+  geom_abline(aes(intercept = .575, slope = .167, linetype = "Average ZLSA")) +
+  geom_abline(aes(intercept = .586, slope = .236, linetype = "+1SD ZLSA")) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid"),
+                        breaks = c("-1SD ZLSA", "Average ZLSA", "+1SD ZLSA"),
+                        name = "Simple Slope") +
+  coord_cartesian(xlim = c(-.20, .60), ylim = c(.1, 1)) +
+  geom_vline(xintercept = -.30) +
+  geom_hline(yintercept = 0) +
+  cleanup + 
+  labs(title="High ZCOS") 
+
+# arrange plots together
+legend = get_legend(plot1)
+hyp2graphout <- plot_grid( plot1 + theme(legend.position="none"),
+                           plot2 + theme(legend.position="none"),
+                           plot3 + theme(legend.position="none"),
+                           legend,
+                           hjust = -1,
+                           nrow = 2
+)
+hyp2graphout
+
+####hyp 3 -- recall####
+overallh3 = glmer(Recall ~ (1|Partno) + Judgment + 
+                    Judged.Value2 + ZCOS * ZLSA * ZFSG,
+                  data = noout,
+                  family = binomial,
+                  control = glmerControl(optimizer = "bobyqa"),
+                  nAGQ = 1)
+summary(overallh3)
+
+####moderation stuff -- recall####
+#low cosine
+lowcos2 = glmer(Recall ~ (1|Partno) +
+                  Judgment + Judged.Value2 + ZCOS_low * ZLSA * ZFSG,
+                data = noout,
+                family = binomial,
+                control = glmerControl(optimizer = "bobyqa"),
+                nAGQ = 1)
+summary(lowcos2)
+
+##high cosine
+hicos2 = glmer(Recall ~ (1|Partno) + 
+                 Judgment + Judged.Value2 + ZCOS_high * ZLSA * ZFSG,
+               data = noout,
+               family = binomial,
+               control = glmerControl(optimizer = "bobyqa"),
+               nAGQ = 1)
+summary(hicos2)
+
+####splitting lsa by cosine strength####
+##low cosine low lsa
+lowcoslowlsa2 = glmer(Recall ~ (1|Partno) + 
+                        Judgment + Judged.Value2 + ZCOS_low * ZLSA_low * ZFSG,
+                      data = noout,
+                      family = binomial,
+                      control = glmerControl(optimizer = "bobyqa"),
+                      nAGQ = 1)
+summary(lowcoslowlsa2)
+
+##low cosine high lsa
+lowcoshighlsa2 = glmer(Recall ~ (1|Partno) + 
+                         Judgment + Judged.Value2 + ZCOS_low * ZLSA_high * ZFSG,
+                       data = noout,
+                       family = binomial,
+                       control = glmerControl(optimizer = "bobyqa"),
+                       nAGQ = 1)
+summary(lowcoshighlsa2)
+
+highcoslowlsa2 = glmer(Recall ~ (1|Partno) + 
+                         Judgment + Judged.Value2 + ZCOS_high * ZLSA_low * ZFSG,
+                       data = noout,
+                       family = binomial,
+                       control = glmerControl(optimizer = "bobyqa"),
+                       nAGQ = 1)
+summary(highcoslowlsa2)
+
+highcoshighlsa2 = glmer(Recall ~ (1|Partno) + 
+                          Judgment + Judged.Value2 + ZCOS_high * ZLSA_high * ZFSG,
+                        data = noout,
+                        family = binomial,
+                        control = glmerControl(optimizer = "bobyqa"),
+                        nAGQ = 1)
+summary(highcoshighlsa2)
+
+####moderation graphs -- recall####
+##low cos
+plot4 = ggplot(dat, aes(x = ZCOS_low, y = Judged.Value2)) +
+  labs(x = "ZFSG", y = "Recall") +
+  scale_size_continuous(guide = FALSE) +
+  geom_abline(aes(intercept = 0.316, slope = 4.116, linetype = "-1SD ZLSA")) +
+  geom_abline(aes(intercept = 0.136, slope = 2.601, linetype = "Average ZLSA")) +
+  geom_abline(aes(intercept = -0.044, slope = 1.086, linetype = "+1SD ZLSA")) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid"),
+                        breaks = c("-1SD ZLSA", "Average ZLSA", "+1SD ZLSA"),
+                        name = "Simple Slope") +
+  coord_cartesian(xlim = c(-.20, .60), ylim = c(.1, 1)) +
+  geom_vline(xintercept = -.30) +
+  geom_hline(yintercept = 0) +
+  cleanup + 
+  labs(title="Low ZCOS") 
+
+##avg cos
+plot5 = ggplot(dat, aes(x = ZCOS_low, y = Judged.Value2)) +
+  labs(x = "ZFSG", y = "Recall") +
+  scale_size_continuous(guide = FALSE) +
+  geom_abline(aes(intercept = 0.369, slope = 3.281, linetype = "-1SD ZLSA")) +
+  geom_abline(aes(intercept = 0.301, slope = 3.084, linetype = "Average ZLSA")) +
+  geom_abline(aes(intercept = 0.234, slope = 2.889, linetype = "+1SD ZLSA")) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid"),
+                        breaks = c("-1SD ZLSA", "Average ZLSA", "+1SD ZLSA"),
+                        name = "Simple Slope") +
+  coord_cartesian(xlim = c(-.20, .60), ylim = c(.1, 1)) +
+  geom_vline(xintercept = -.30) +
+  geom_hline(yintercept = 0) +
+  cleanup + 
+  labs(title="Average ZCOS") 
+
+##high cos
+plot6 = ggplot(dat, aes(x = ZCOS_low, y = Judged.Value2)) +
+  labs(x = "ZFSG", y = "Recall") +
+  scale_size_continuous(guide = FALSE) +
+  geom_abline(aes(intercept = .421, slope = 2.44, linetype = "-1SD ZLSA")) +
+  geom_abline(aes(intercept = .466, slope = 3.569, linetype = "Average ZLSA")) +
+  geom_abline(aes(intercept = .511, slope = 4.692, linetype = "+1SD ZLSA")) +
+  scale_linetype_manual(values = c("dotted", "dashed", "solid"),
+                        breaks = c("-1SD ZLSA", "Average ZLSA", "+1SD ZLSA"),
+                        name = "Simple Slope") +
+  coord_cartesian(xlim = c(-.20, .60), ylim = c(.1, 1)) +
+  geom_vline(xintercept = -.30) +
+  geom_hline(yintercept = 0) +
+  cleanup + 
+  labs(title="High ZCOS") 
+
+legend = get_legend(plot4)
+hyp3graphout <- plot_grid( plot4 + theme(legend.position="none"),
+                           plot5 + theme(legend.position="none"),
+                           plot6 + theme(legend.position="none"),
+                           legend,
+                           hjust = -1,
+                           nrow = 2
+)
+hyp3graphout
